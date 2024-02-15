dbd96d3c06cad60209355645d3e68bf41e06ea61
Annie Ehler <annie.ehler.4@gmail.com>
2024-02-15 10:56:36 -0800

    Minimal configurations for servers

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PWD="$(pwd)"
+
+cp -R "${PWD}/config/bat" ~/.config/bat
+cp -R "${PWD}/config/cheat" ~/.config/cheat
+cp -R "${PWD}/config/gh-changelog" ~/.config/gh-changelog
+cp -R "${PWD}/config/dh-dash" ~/.config/dh-dash
+cp -R "${PWD}/config/lazygit" ~/.config/lazygit
+cp -R "${PWD}/config/starship.toml" ~/.config/starship.toml
+
+git clone git@github.com:annie444/nvim-dots.git ~/.config/nvim
+mkdir -p ~/.local/share/nvim/lazy
+git clone --filter=blob:none git@github.com:folke/lazy.nvim.git --branch=stable ~/.local/share/nvim/lazy/lazy.nvim
